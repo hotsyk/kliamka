6ea565d539572ea9fc49d2e6e4445eb372b70b64
Volodymyr Hotsyk <git@hotsyk.com>
2025-06-08 13:45:13 -0700

    Publish pypi package only if version changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kliamka",
-    version="0.1.2",
+    version="0.1.3",
     author="Volodymyr Hotsyk",
     author_email="git@hotsyk.com",
     description="Small Python CLI library",
