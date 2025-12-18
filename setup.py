f59548493db5ffac08c117bed226dfbd6e7732a8
Volodymyr Hotsyk <git@hotsyk.com>
2025-12-18 15:51:49 -0800

    Update version to 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kliamka",
-    version="0.3.0",
+    version="0.4.0",
     author="Volodymyr Hotsyk",
     author_email="git@hotsyk.com",
     description="Small Python CLI library",
